82c1f6d0e8b2e72c6562f040df307a56606f669c
Anna Cecilia <ac@madebyupstate.com>
2020-01-16 21:09:10 -0500

    added banner and stylized, linked javascript file

---
+++ assets/main.js
@@ -0,0 +1,58 @@
+//Elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunct = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+// Generate event listen
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+})
+
+// Generate Password function
+function generatePassword(lower,upper,number,symbol,length) {
+    // password variable, filter out unchecked types, look over length, call generator function for each type, add final password to password variable and return it
+
+    let generatedPassword = " ";
+
+    const typeCount = lower + upper + number + symbol;
+
+    console.log ("typesCount: ", typeCount);
+}
+
+// Generator Functions
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
